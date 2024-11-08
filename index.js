0dd41ee0718fedf8819a893d7b9d7621dfff642d
Dmytr0-kova1 <kdmytro793@gmail.com>
2024-11-08 20:44:52 +0000

    Deploying to gh-pages from @ Dmytr0-kova1/practik@d07e01d90c5579b4eff13f292b3e0292e7bd376b 🚀

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import{a as n}from"./assets/vendor-CNNbG8jS.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();n.defaults.baseURL="https://dummyjson.com/recipes";async function u(){const{data:r}=await n();return r}const l={recipesList:document.querySelector(".recipes-list")};function a(r){const o=r.map(({image:s,name:i,difficulty:e,cuisine:t})=>`
+import{a as n}from"./assets/vendor-CNNbG8jS.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();n.defaults.baseURL="https://dummyjson.com/recipes";async function d(){const{data:t}=await n();return t}async function u(t){const{data:o}=await n(`/${t}`);return o}const a={recipesList:document.querySelector(".recipes-list"),backdrop:document.querySelector(".backdrop")};function l(){a.backdrop.classList.remove("hidden")}function p(t){const o=t.map(({image:s,name:i,difficulty:e,cuisine:r,id:c})=>`
     <li class="recipes-item">
-          <img src="${s}" alt="${i}" />
+          <img src="${s}" alt="${i}" data-id="${c}"/>
           <div>
             <h3>${i}</h3>
-            <p>Cuisine: ${t}</p>
+            <p>Cuisine: ${r}</p>
             <p>Difficulty: ${e}</p>
           </div>
         </li>
-    `).join("");l.recipesList.innerHTML=o}async function d(){try{const{recipes:r}=await u();a(r)}catch(r){console.log("error",r)}}document.addEventListener("DOMContentLoaded",d);
+    `).join("");a.recipesList.innerHTML=o}async function f(){try{const{recipes:t}=await d();p(t)}catch(t){console.log("error",t)}}async function m(t){if(t.target.nodeName==="IMG")try{const o=t.target.dataset.id,s=await u(o);l()}catch(o){console.log(o)}}document.addEventListener("DOMContentLoaded",f);a.recipesList.addEventListener("click",m);
 //# sourceMappingURL=index.js.map
